7e6e81476be2a8bc88312e592273f5d274ac3626
Andrew <alevy97@gmail.com>
2023-06-21 15:25:29 -0400

    initial commit

--- README.md
+++ README.md
@@ -5,27 +5,38 @@ Apply styles directly to your React Native components via props!
 ## Installation
 
 ```sh
-npm install style-direct-club
+yarn add style-direct-club
 ```
 
 ## Usage
 
 ```js
-import { multiply } from 'style-direct-club';
+import { Text, View } from 'react-native';
+import { styled } from 'style-direct-club';
 
-// ...
+const StyledText = styled(Text);
+const StyledView = styled(View);
 
-const result = await multiply(3, 7);
+function App() {
+  return (
+    <StyledView
+      flex={1}
+      rowGap={10}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <StyledText color="red" fontSize={24}>
+        Hello World!
+      </StyledText>
+      <StyledText color="blue" fontSize={16}>
+        Styling is so easy!
+      </StyledText>
+    </StyledView>
+  );
+}
 ```
 
-## Contributing
+## To Do
 
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
-
-## License
-
-MIT
-
----
-
-Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
+- [ ] Add support for aliases (e.g. `backgroundColor` -> `bg`)
+- [ ] Add out of the box components
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,145 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import { ComponentProps, createElement } from 'react';
+
+/**
+ * A helper function to add style props to a component. This isn't necessary,
+ * but it makes it easier to style components.
+ *
+ * This turns this:
+ * ```
+ * <Text style={{ color: "red", marginTop: 10 }} />
+ * ```
+ *
+ * Into this:
+ * ```
+ * <Text color="red" marginTop={10} />
+ * ```
+ *
+ */
+export function createStyledComponent<T extends React.ComponentType<any>>(
+  component: T
+) {
+  type AllProps = ComponentProps<T>;
+  type MappedStyleProps = AllProps extends { style?: infer S } ? S : {};
+
+  return function StyledComponent(props: AllProps & MappedStyleProps) {
+    return createElement(component, {
+      ...props,
+      style: mapStyleProps(props),
+    });
+  };
 }
+
+function mapStyleProps(props: Record<string, any>) {
+  const styleProps: Record<string, any> = {};
+  if (props) {
+    Object.keys(props).forEach((key) => {
+      if (allowedProps.includes(key)) {
+        styleProps[key] = props[key];
+      }
+    });
+  }
+  return styleProps;
+}
+
+const allowedProps = [
+  'alignContent',
+  'alignItems',
+  'alignSelf',
+  'aspectRatio',
+  'borderBottomWidth',
+  'borderEndWidth',
+  'borderLeftWidth',
+  'borderRightWidth',
+  'borderStartWidth',
+  'borderTopWidth',
+  'borderRadius',
+  'borderWidth',
+  'bottom',
+  'display',
+  'end',
+  'flex',
+  'flexBasis',
+  'flexDirection',
+  'rowGap',
+  'gap',
+  'columnGap',
+  'flexGrow',
+  'flexShrink',
+  'flexWrap',
+  'height',
+  'justifyContent',
+  'left',
+  'margin',
+  'marginBottom',
+  'marginEnd',
+  'marginHorizontal',
+  'marginLeft',
+  'marginRight',
+  'marginStart',
+  'marginTop',
+  'marginVertical',
+  'maxHeight',
+  'maxWidth',
+  'minHeight',
+  'minWidth',
+  'overflow',
+  'padding',
+  'paddingBottom',
+  'paddingEnd',
+  'paddingHorizontal',
+  'paddingLeft',
+  'paddingRight',
+  'paddingStart',
+  'paddingTop',
+  'paddingVertical',
+  'position',
+  'right',
+  'start',
+  'top',
+  'width',
+  'zIndex',
+  'direction',
+  'shadowColor',
+  'shadowOffset',
+  'shadowOpacity',
+  'shadowRadius',
+  'backgroundColor',
+  'borderBottomColor',
+  'borderBottomEndRadius',
+  'borderBottomLeftRadius',
+  'borderBottomRightRadius',
+  'borderBottomStartRadius',
+  'borderColor',
+  'borderEndColor',
+  'borderLeftColor',
+  'borderRightColor',
+  'borderStartColor',
+  'borderStyle',
+  'borderTopColor',
+  'borderTopEndRadius',
+  'borderTopLeftRadius',
+  'borderTopRightRadius',
+  'borderTopStartRadius',
+  'opacity',
+  'elevation',
+  'fontVariant',
+  'letterSpacing',
+  'textDecorationColor',
+  'textDecorationStyle',
+  'writingDirection',
+  'color',
+  'fontFamily',
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'lineHeight',
+  'textAlign',
+  'textDecorationLine',
+  'textShadowColor',
+  'textShadowOffset',
+  'textShadowRadius',
+  'textTransform',
+  'resizeMode',
+  'overlayColor',
+  'tintColor',
+];
